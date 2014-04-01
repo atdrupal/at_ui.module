aea7e21cb93efee54ac252926d918bdf3492dde4
Andy Truong <andy@toila.net>
2014-04-01 12:11:04 +1000

    + /at/icon

---
+++ lib/Controller/Icons.php
@@ -0,0 +1,48 @@
+<?php
+namespace Drupal\at_ui\Controller;
+
+class Icons {
+  public function renderServices() {
+    $rows = array();
+
+    foreach (at_container('container')->find('at.icon', 'service') as $name => $service) {
+      foreach ($service->getIconSets() as $set_name) {
+        $rows[] = array(l($service->getName(), "at/icon/{$name}"), l($set_name, "at/icon/{$name}/{$set_name}"));
+      }
+    }
+
+    return array('#theme' => 'table', '#header' => array('Service', 'Set'), '#rows' => $rows);
+  }
+
+  /**
+   * Callback for /at/icon/%/%
+   *
+   * @param  string $name     Service name.
+   * @param  string $set_name
+   * @return array
+   */
+  public function renderServiceSet($name, $set_name) {
+    $service  = at_container($name);
+    $items = array();
+
+    drupal_set_title("{$set_name} — " . $service->getName());
+
+    foreach ($service->getIconList($set_name) as $icon_name) {
+      $items[] = str_replace(
+        '<i',
+        '<i data-name="'. $icon_name .'"',
+        $service->get("{$set_name}/{$icon_name}")->render()
+      );
+    }
+
+    return array(
+      '#theme' => 'item_list',
+      '#items' => $items,
+      '#attributes' => array(
+        'class' => array('at-icon-list'),
+        'data-service' => $name,
+        'data-set' => $set_name,
+      ),
+    );
+  }
+}
---
+++ misc/js/at.icon.js
@@ -0,0 +1,40 @@
+(function($){
+
+var icon_info = function(service, icon_set, name) {
+  return ''
+    + '<table style="width: 100%;">'
+    + '  <tr><td><strong>Service:</strong></td> <td>'+ service +'</td></tr>'
+    + '  <tr><td><strong>Set:</strong></td>     <td>'+ icon_set +'</td></tr>'
+    + "  <tr><td><strong>PHP:</strong></td>     <td><code>&lt;?php at_icon('"+ icon_set +"/"+ name +"', '"+ service +"') ?&gt;</code></td></tr>"
+    + "  <tr><td><strong>Twig:</strong></td>    <td><code>{{ '"+ icon_set +"/"+ name +"'|icon('"+ service +"') }}</code></td></tr>"
+    + '</table>';
+};
+
+Drupal.behaviors.atuiIcon = {
+  attach: function(context, settings) {
+    var service  = $('.at-icon-list').data('service');
+    var icon_set = $('.at-icon-list').data('set');
+
+    $('.at-icon-list li i', context).once('atuiIcon')
+      .append(function(){ return '<span>' + $(this).data('name') + '</span>'; })
+      .click(function(){
+        var name = $(this).data('name');
+        var icon = $(this).parent().html();
+        var title = icon + ' — ' + service + '/' + icon_set;
+        $(icon_info(service, icon_set, name)).dialog({
+          title: title,
+          width: '800px',
+          open: function() {
+            // $(this).css('width', '100%');
+            $(icon)
+              .find('span').remove().end()
+              .css({'font-size': '5em', 'float': 'right', 'padding': '33px'})
+              .insertBefore($(this));
+          }
+        });
+      })
+    ;
+  }
+};
+
+})(jQuery);
