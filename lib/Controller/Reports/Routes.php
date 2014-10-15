92614fcd3295bf36f25eb42b4c971266f75d1923
Andy Truong <thehongtt@gmail.com>
2014-10-15 16:26:11 +0700

    Include 'Flush compiled Twig templates' button at /at/twig.

--- lib/Controller/Reports/Routes.php
+++ lib/Controller/Reports/Routes.php
@@ -27,7 +27,7 @@ class Routes {
         }
         $breadcrumbs = array_merge($breadcrumbs, $this->findExternalBreadcrumbs($path));
 
-        $rows[] = array($module, $path, go1dr($route), atdr($attached), atdr($blocks), atdr($breadcrumbs));
+        $rows[] = array($module, $path, atdr($route), atdr($attached), atdr($blocks), atdr($breadcrumbs));
       }
     }
 
