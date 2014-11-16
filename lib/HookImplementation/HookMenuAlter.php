bd2ab669f74aaf2f9c5351ebff1686774b22d69d
Andy Truong <thehongtt@gmail.com>
2014-11-16 10:00:17 +0700

    Clean up at_ui_menu_alter().

---
+++ lib/HookImplementation/HookMenuAlter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\at_ui\HookImplementation;
+
+class HookMenuAlter
+{
+
+    public function execute(array &$menu)
+    {
+        $this->alterDevelTabs($menu);
+    }
+
+    private function alterDevelTabs(&$menu)
+    {
+        $keys = array();
+        $keys[] = 'user/%user/devel';
+        $keys[] = 'node/%node/devel';
+        $keys[] = 'comment/%comment/devel';
+        $keys[] = 'taxonomy/term/%taxonomy_term/devel';
+        if (isset($menu['block/%bean_delta/devel'])) {
+            $keys[] = 'block/%bean_delta/devel';
+        }
+
+        foreach ($keys as $key) {
+            $position = 0 === strpos(arg(1, $key), '%') ? 1 : 2;
+
+            // Aler */load tab
+            $menu[$key]['page callback'] = 'atdr';
+            $menu[$key]['page arguments'] = array($position);
+
+            // Aler */render tab
+            $menu[$key . '/render']['page callback'] = 'Drupal\at_ui\HookImplementation\HookMenuAlter::renderObject';
+        }
+    }
+
+    public static function renderObject($entity_type, $entity, $name = NULL)
+    {
+        return atdr($entity);
+    }
+
+}
